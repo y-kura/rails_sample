6cdf9484036b83643be0e0308273590aa633d3d4
y-kura <56643522+y-kura@users.noreply.github.com>
2019-10-17 00:36:01 +0900

    不要なファイルを削除

--- app/models/mini_board_message.rb
+++
@@ -1,9 +0,0 @@
-class Message < ActiveRecord::Base
-  validates :name,
-    presence: {:message =>'名前を入力してください'},
-    length: {:maximum => 16, :message => '名前は16文字以内にしてください'}
- 
-  validates :body,
-    presence: {:message =>'メッセージを入力してください'},
-    length: {:maximum => 999, :message => 'メッセージは999文字以内にしてください'}
-end
